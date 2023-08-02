e3ffbb428e110068437213f34fdcd4b65c2494c5
rarenight <115183461+rarenight@users.noreply.github.com>
2023-08-02 15:57:48 -0400

    Update dom-hasher.js

--- dom-hasher.js
+++ dom-hasher.js
@@ -6,8 +6,10 @@
 // @description  Auto-hashes and auto-inputs the Size, CRC32, MD5, SHA-1, and SHA-256 fields when you drag and drop a file onto the Submit File page in No-Intro's DAT-o-MATIC
 // @match        https://datomatic.no-intro.org/*
 // @grant        none
-// @require      https://cdn.jsdelivr.net/npm/hash-wasm@4/dist/md5.umd.min.js
 // @require      https://cdn.jsdelivr.net/npm/hash-wasm@4/dist/crc32.umd.min.js
+// @require      https://cdn.jsdelivr.net/npm/hash-wasm@4/dist/md5.umd.min.js
+// @require      https://cdn.jsdelivr.net/npm/hash-wasm@4/dist/sha1.umd.min.js
+// @require      https://cdn.jsdelivr.net/npm/hash-wasm@4/dist/sha256.umd.min.js
 // ==/UserScript==
 /* globals hashwasm */
 
@@ -34,7 +36,6 @@
     window.ondrop = async (event) => {
       event.preventDefault();
 
-
       function setFormValue(fieldName, value) {
         const fieldElement = document.querySelector(fieldName);
         if (fieldElement) {
@@ -42,9 +43,19 @@
         }
       }
 
-      async function createHash(algorithm, buffer) {
-        const hashBuffer = await crypto.subtle.digest(algorithm, buffer);
-        return Array.from(new Uint8Array(hashBuffer)).map((b) => b.toString(16).padStart(2, '0')).join('');
+      async function createHash(hashFunc, file) {
+        const hasher = await hashFunc();
+        hasher.init();
+        const reader = file.stream().getReader();
+        while (true) {
+          const { done, value } = await reader.read();
+          if (done) {
+            break;
+          }
+          hasher.update(new Uint8Array(value));
+        }
+        const hash = hasher.digest('hex');
+        return hash;
       }
 
       const files = event.dataTransfer.files;
@@ -55,31 +66,24 @@
           continue;
         }
 
-        if (file.size > 2 * 1024 * 1024 * 1024) {
-          alert("This file is too large to hash. Please use a dedicated hashing program instead.");
-          continue;
-        }
-
         setFormValue(`input[name="f[${currentFormIndex}][file_size]"]`, "Calculating...");
         setFormValue(`input[name="f[${currentFormIndex}][file_crc32]"]`, "Calculating...");
         setFormValue(`input[name="f[${currentFormIndex}][file_md5]"]`, "Calculating...");
         setFormValue(`input[name="f[${currentFormIndex}][file_sha1]"]`, "Calculating...");
         setFormValue(`input[name="f[${currentFormIndex}][file_sha256]"]`, "Calculating...");
 
-        const buffer = await file.arrayBuffer();
-
         setFormValue(`input[name="f[${currentFormIndex}][file_size]"]`, file.size);
 
-        const crc32 = await hashwasm.crc32(new Uint8Array(buffer));
+        const crc32 = await createHash(hashwasm.createCRC32, file);
         setFormValue(`input[name="f[${currentFormIndex}][file_crc32]"]`, crc32);
 
-        const md5 = await hashwasm.md5(new Uint8Array(buffer));
+        const md5 = await createHash(hashwasm.createMD5, file);
         setFormValue(`input[name="f[${currentFormIndex}][file_md5]"]`, md5);
 
-        const sha1 = await createHash('SHA-1', buffer);
+        const sha1 = await createHash(hashwasm.createSHA1, file);
         setFormValue(`input[name="f[${currentFormIndex}][file_sha1]"]`, sha1);
 
-        const sha256 = await createHash('SHA-256', buffer);
+        const sha256 = await createHash(hashwasm.createSHA256, file);
         setFormValue(`input[name="f[${currentFormIndex}][file_sha256]"]`, sha256);
 
         currentFormIndex = 1 - currentFormIndex;
